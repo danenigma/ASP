8d7b6be1e8f0777f356ac4eee10aa3925515d419
danenigma <danenigma@users.noreply.github.com>
2017-09-16 05:07:27 -0700

    q1 a done

---
+++ hm3/compute_data_rate_range_and_mean.m
@@ -0,0 +1,10 @@
+
+function [linkBinoPdf,linkBinoCdf,data_rate_range_95,mu] = compute_data_rate_range_and_mean(N,P,Rb)
+    possibleNumberOfUsers = 0:N;
+    linkBinoPdf = binopdf(possibleNumberOfUsers,N,P);%get a binomial dist 
+    mu = sum(linkBinoPdf.*possibleNumberOfUsers);% compute the mean of the base design
+    linkBinoCdf = binocdf(possibleNumberOfUsers,N,P);% compute the cdf
+    number_of_u_95  = possibleNumberOfUsers(find(linkBinoCdf>=0.95, 1 ));
+    number_of_u_5   = possibleNumberOfUsers(find(linkBinoCdf<=0.05, 1, 'last' ));
+    data_rate_range_95 = [round(Rb/number_of_u_95),round(Rb/number_of_u_5)];
+end
--- hm3/q1.m
+++ hm3/q1.m
@@ -1,21 +1,34 @@
 
 %(a)
-possibleNumberOfUsers = 0:100;%number of users 0-100
+DEBUG = 0;
 Rb = 1000;%Kbps
 Nb = 100;%max number of users
 Pb = 0.3;%probability of being active
-linkBinoPdf = binopdf(possibleNumberOfUsers,Nb,Pb);%get a binomial dist 
-stem(possibleNumberOfUsers,linkBinoPdf);%ploting pdf
-mub = sum(linkBinoPdf.*possibleNumberOfUsers);% compute the mean of the base design
+[linkBinoPdf,linkBinoCdf,data_rate_range_95,mub]=compute_data_rate_range_and_mean(Nb,Pb,Rb);
+figure;
+stem(0:Nb,linkBinoPdf);
 fprintf('The average number active users our base design is : %2.2f \n',mub);
-linkBinoCdf = binocdf(possibleNumberOfUsers,Nb,Pb);% compute the cdf
 hold on;
-plot(possibleNumberOfUsers,linkBinoCdf);
+plot(0:Nb,linkBinoCdf);
 hold off;
-number_of_u_95  = possibleNumberOfUsers(min(find(linkBinoCdf>=0.95)));
-number_of_u_5   = possibleNumberOfUsers(max(find(linkBinoCdf<=0.05)));
-data_rate_range = [round(Rb/number_of_u_95),round(Rb/number_of_u_5)];
 fprintf('The data rates that will be experienced by users 95 percent of the time range from %i kbps to %i kbps\n',...
-    data_rate_range(1),data_rate_range(2));
+    data_rate_range_95(1),data_rate_range_95(2));
 
 %(b)
+%mu1 = mub -> N1P1 = NbPb -> P1 = NbPb/N1
+Rb = 1000;%Kbps
+N1 = 100:100:10000;
+data_rate_ranges = zeros(length(N1),2);
+
+for i = 1:length(N1)
+  
+    P1 = Nb*Pb/N1(i);% P1 ->0
+    [linkBinoPdf,linkBinoCdf,data_rate_range_95,mu1]=compute_data_rate_range_and_mean(N1(i),P1,Rb);
+    data_rate_ranges(i,:) = data_rate_range_95;
+    
+    if DEBUG
+    fprintf('The average number active users for Design 1 is : %2.2f \n',mu1);
+    fprintf('The data rates that will be experienced by users 95 percent of the time ranges from %i kbps to %i kbps\n',...
+        data_rate_range_95(1),data_rate_range_95(2));
+    end
+end
